3cc870048e766f8d58e08abd78b51fe95100979a
Xiuer0x0 <float800508@hotmail.com>
2019-11-08 16:25:39 +0800

    class 使用範例

---
+++ Some.js
@@ -0,0 +1,50 @@
+class Animal {
+    constructor(name) {
+        this.name = (name || 'Unknown animal');
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    say(message) {
+        const content = `${this.name}: ${message}`;
+        console.log(content);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, type) {
+        super(name)
+        this.type = (type || '');
+    }
+
+    getType() {
+        return this.type;
+    }
+}
+
+class Cat extends Animal {
+    /**
+     * @param {String[]} eatList
+     */
+    constructor(name, eatList) {
+        super(name)
+        this.eatList = (eatList || ['']);
+    }
+
+    getEatList() {
+        return this.eatList;
+    }
+}
+
+const tiger = new Animal('ttt');
+const dog = new Dog('foo', '土狗');
+const cat = new Cat('www', ['魚', '罐罐']);
+
+tiger.say('吼');
+dog.say('汪');
+cat.say('喵');
+
+console.log(`${dog.getName()} 的品種是 ${dog.getType()}.`);
+console.log(`${cat.getName()} 會吃的食物有： ${cat.getEatList().join(', ')}.`);
